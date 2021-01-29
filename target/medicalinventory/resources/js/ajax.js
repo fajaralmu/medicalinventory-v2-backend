15551c092dec9c0d2ec88135fe9613481a97e215
Fajar AM <somabangsa@gmail.com>
2021-01-29 10:21:51 +0700

    gitignore

--- target/medicalinventory/resources/js/ajax.js
+++
@@ -1,9 +0,0 @@
-function commonAuthHeader() {
-	const header = {};
-	if(document.getElementById("jwt_token")) {
-		header["Authorization"] = "Bearer "+ document.getElementById("jwt_token").value;
-	}
-	
-	return header;
-	
-} 
