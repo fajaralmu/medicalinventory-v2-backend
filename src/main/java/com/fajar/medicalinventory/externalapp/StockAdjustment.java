f3649bd496fafe9fc3941a9d940977b2851f0473
Fajar AM <somabangsa@gmail.com>
2021-02-03 15:02:42 +0700

    add script adjust stocks

--- src/main/java/com/fajar/medicalinventory/externalapp/StockAdjustment.java
+++ src/main/java/com/fajar/medicalinventory/externalapp/StockAdjustment.java
@@ -1,5 +1,6 @@
 package com.fajar.medicalinventory.externalapp;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -15,30 +16,139 @@ public class StockAdjustment {
 
 	static Session session;
 	static Map<Long, ProductFlow> productFlowMap = new HashMap<Long, ProductFlow>();
-	
-	public static void main(String[] args) {
+
+	public static void recalculateStock(){
+
 		session = HibernateSessions.setSession();
-		Criteria criteria = session.createCriteria(ProductFlow.class);
-		criteria.add(Restrictions.isNotNull("referenceProductFlow"));
-		List list = criteria.list();
-		for (Object object : list) {
-			ProductFlow productFlow = (ProductFlow) object;
-			ProductFlow reference = productFlow.getReferenceProductFlow();
-			if (null == productFlowMap.get(reference.getId())) {
-				productFlowMap.put(reference.getId(), reference);
+		Transaction tx = null;
+		try {
+
+			tx = session.beginTransaction();
+			Criteria criteriaSupply = session.createCriteria(ProductFlow.class);
+			criteriaSupply.add(Restrictions.isNull("referenceProductFlow"));
+			List productSupplyFlows = criteriaSupply.list();
+			for (Object productSupplyFlow : productSupplyFlows) {
+				ProductFlow pf = (ProductFlow) productSupplyFlow;
+				pf.resetUsedCount();
+				productFlowMap.put(pf.getId(), pf);
 			}
-			productFlowMap.get(reference.getId()).addUsedCount(productFlow.getCount());
+
+			Criteria criteriaUsed = session.createCriteria(ProductFlow.class);
+			criteriaUsed.add(Restrictions.isNotNull("referenceProductFlow"));
+			List productUsedFlows = criteriaSupply.list();
+
+			for (Object object : productUsedFlows) {
+				ProductFlow pf = (ProductFlow) object;
+				productFlowMap.get(pf.getReferenceProductFlow().getId()).addUsedCount(pf.getCount());
+
+			}
+			for (Long id : productFlowMap.keySet()) {
+				session.merge(productFlowMap.get(id));
+
+			}
+			tx.commit();
+		} catch (Exception e) {
+			if (tx != null) {
+				tx.rollback();
+			}
+		} finally {
+			session.close();
+			System.exit(0);
+		}
+	}
+	/**
+	 * adjust transaction dates
+	 * 
+	 */
+	public static void main(String[] args) {
+		String id_raw = "5357\r\n" + 
+				"5829";
+		session = HibernateSessions.setSession();
+		Transaction tx = session.beginTransaction();
+		String[] flowids = id_raw.split("\r\n");
+		List<ProductFlow> flows = new ArrayList<>();
+		for (int i = 0; i < flowids.length; i++) {
+			ProductFlow pf = (ProductFlow) session.get(ProductFlow.class, Long.valueOf(flowids[i]));
+			flows.add(pf);
+		}
+		Map<Long, com.fajar.medicalinventory.entity.Transaction> editedTranMap = new HashMap<>();
+		for (ProductFlow productFlow : flows) {
+			com.fajar.medicalinventory.entity.Transaction refTransactionSupply = productFlow.getReferenceProductFlow().getTransaction();
+			com.fajar.medicalinventory.entity.Transaction refTransaction = productFlow .getTransaction();
+			System.out.println("refTransactionSupply:"+refTransactionSupply.getId()+" at "+refTransactionSupply.getTransactionDate());
+			System.out.println("refTransaction: "+refTransaction.getId()+" at "+refTransaction.getTransactionDate());
+			boolean usedBeforeSupply = refTransaction.getTransactionDate().before(refTransactionSupply.getTransactionDate());
+			System.out.println("usedBeforeSupply: "+usedBeforeSupply);
+			System.out.println("");
+			if (null == editedTranMap.get(refTransactionSupply.getId()) ) {
+				editedTranMap.put(refTransactionSupply.getId(), refTransactionSupply);
+//				session.merge(refTransactionSupply);
+			}
+			if (editedTranMap.get(refTransactionSupply.getId()).getTransactionDate().after( refTransaction.getTransactionDate())) {
+				System.out.println("WILL UPDATE");
+				editedTranMap.get(refTransactionSupply.getId()).setTransactionDate(refTransaction.getTransactionDate());
+			} else {
+				System.out.println("##NO UPDATE");
+			}
 			
 		}
-		
-		Transaction tx = session.beginTransaction();
-		for (Long id : productFlowMap.keySet()) {
-			session.merge(productFlowMap.get(id));
-			System.out.println("saved: "+id);
-		}
-		
 		tx.commit();
 		session.close();
 		System.exit(0);
 	}
+	
+	static final String FLOW_IDS = "5492\r\n" + 
+			"5507\r\n" + 
+			"5471\r\n" + 
+			"5571\r\n" + 
+			"5570\r\n" + 
+			"5578\r\n" + 
+			"5753\r\n" + 
+			"5511\r\n" + 
+			"5501\r\n" + 
+			"5727\r\n" + 
+			"5799\r\n" + 
+			"5490\r\n" + 
+			"5751\r\n" + 
+			"5843\r\n" + 
+			"5844\r\n" + 
+			"5845\r\n" + 
+			"5494\r\n" + 
+			"5529\r\n" + 
+			"5754\r\n" + 
+			"5432\r\n" + 
+			"1468\r\n" + 
+			"1469\r\n" + 
+			"5433\r\n" + 
+			"5491\r\n" + 
+			"5495\r\n" + 
+			"5508\r\n" + 
+			"5435\r\n" + 
+			"5434\r\n" + 
+			"5553\r\n" + 
+			"5552\r\n" + 
+			"5568\r\n" + 
+			"5745\r\n" + 
+			"5461\r\n" + 
+			"5456\r\n" + 
+			"5732\r\n" + 
+			"5500\r\n" + 
+			"5506\r\n" + 
+			"5532\r\n" + 
+			"5484\r\n" + 
+			"5493\r\n" + 
+			"5739\r\n" + 
+			"5583\r\n" + 
+			"5431\r\n" + 
+			"5738\r\n" + 
+			"5441\r\n" + 
+			"5581\r\n" + 
+			"5771\r\n" + 
+			"5569\r\n" + 
+			"5725\r\n" + 
+			"5604\r\n" + 
+			"5804\r\n" + 
+			"1467";
+	
+	
 }
