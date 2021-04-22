3f0f9952136c8c3472b82b4f0a0bf9bfea6fee9d
Fajar AM <somabangsa@gmail.com>
2021-04-22 08:02:21 +0700

    update front end, improve performance

--- src/main/java/com/fajar/medicalinventory/service/RealtimeService2.java
+++ src/main/java/com/fajar/medicalinventory/service/RealtimeService2.java
@@ -7,46 +7,43 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import com.fajar.medicalinventory.dto.WebResponse;
+import com.fajar.medicalinventory.util.ThreadUtil;
 
 @Service
 public class RealtimeService2 {
 	Logger log = LoggerFactory.getLogger(RealtimeService2.class);
 
 	@Autowired
-	private SimpMessagingTemplate webSocket;  
+	private SimpMessagingTemplate webSocket;
 
 	public RealtimeService2() {
 		LogProxyFactory.setLoggers(this);
-		log.info("=======================REALTIME SERVICE 2======================="); 
+		log.info("=======================REALTIME SERVICE 2=======================");
 	}
 
-
 	public boolean sendUpdateSession(Object payload) {
- 
+
 		webSocket.convertAndSend("/wsResp/sessions", payload);
 
 		return true;
 	}
 
 	public void sendProgress(double progress, String requestId) {
-	//	System.out.println(">>>>>>>>>>SEND PROGRESS:" + progress + " (" + requestId + ")");
 		sendProgress(WebResponse.builder().requestId(requestId).percentage(progress).build());
 	}
 
 	public void sendProgress(WebResponse WebResponse) {
-		webSocket.convertAndSend("/wsResp/progress/"+WebResponse.getRequestId(), WebResponse);
+		ThreadUtil.run(() -> {
+			webSocket.convertAndSend("/wsResp/progress/" + WebResponse.getRequestId(), WebResponse);
+		});
 	}
-
 
 	public void sendMessageChatToClient(WebResponse response, String requestId) {
-		webSocket.convertAndSend("/wsResp/messages/"+requestId, response); 
+		webSocket.convertAndSend("/wsResp/messages/" + requestId, response);
 	}
-	 
- 
+
 	public void sendChatMessageToAdmin(WebResponse response) {
-		webSocket.convertAndSend("/wsResp/adminmessages", response); 
+		webSocket.convertAndSend("/wsResp/adminmessages", response);
 	}
-	
-	
 
 }
